803c9e5c97eeeb9127c31d6b40507070fc22c609
LuMo569 <100814672+LuMo569@users.noreply.github.com>
2025-01-20 00:08:15 +0100

    Update DatabaseDriver.java

--- src/main/java/com/example/cashiersystem/Model/DatabaseDriver.java
+++ src/main/java/com/example/cashiersystem/Model/DatabaseDriver.java
@@ -6,9 +6,9 @@ import java.sql.*;
 import java.util.*;
 
 public class DatabaseDriver {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3360/datenbank_boniersystem";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Jsb9yfgMKZ58h5tj";
+    private static final String URL = "YourURL";
+    private static final String USER = "YoutUser";
+    private static final String PASSWORD = "YourPassword";
 
     /**
      *Establishes a connection to the database using the provided credentials.
