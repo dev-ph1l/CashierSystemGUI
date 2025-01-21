86c3632a805f9b9d99e5669f9347f555f5774749
Admin <104310739+dev-ph1l@users.noreply.github.com>
2025-01-21 23:58:42 +0100

    Dynamically add buttons to the ItemPicker

--- src/main/java/com/example/cashiersystem/Controllers/Waiter/ItemPickerController.java
+++ src/main/java/com/example/cashiersystem/Controllers/Waiter/ItemPickerController.java
@@ -9,57 +9,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ItemPickerController implements Initializable {
-    // drinks
-    public Button water_03_btn;
-    public Button water_05_btn;
-    public Button sparkling_water_03_btn;
-    public Button sparkling_water_05_btn;
-    public Button cola_btn;
-    public Button sprite_btn;
-    public Button fanta_btn;
-    public Button ice_tea_peach_btn;
-    public Button ice_tea_lemon_btn;
-    public Button energy_drink_btn;
-    public Button smoothie_btn;
-    public Button herbal_tea_btn;
-    public Button hot_chocolate_btn;
-    public Button cappuccino_btn;
-    public Button draft_beer_btn;
-    public Button wheat_beer_btn;
-    public Button aperol_spritz_btn;
-    public Button mojito_btn;
-    public Button gin_tonic_btn;
-    public Button long_island_iced_tea_btn;
-    public Button whisky_sour_btn;
-    public Button red_wine_btn;
-    public Button white_wine_btn;
-    public Button tequila_shot_btn;
-    public Button espresso_btn;
-    public Button pina_colada_btn;
-    public Button cosmopolitan_btn;
-    public Button caipirinha_btn;
+    private final Map<String, Integer> buttonIdMap = new HashMap<>();
 
-    // food
-    public Button burger_btn;
-    public Button burger_witch_fries_btn;
-    public Button chicken_wings_btn;
-    public Button club_sandwich_btn;
-    public Button nachos_cheese_btn;
-    public Button ceaser_salad_btn;
-    public Button mini_pizzas_btn;
-    public Button cheese_platter_btn;
-    public Button peanuts_btn;
-    public Button pretzel_sticks_btn;
-    public Button chips_btn;
-
-    private final Map<Button, Integer> buttonIdMap = new HashMap<>();
+    public GridPane food_items;
 
 
     // control buttons
@@ -83,12 +42,6 @@ public class ItemPickerController implements Initializable {
         // register Buttons
         registerButtons();
 
-        // Set up event listeners for each button
-        buttonIdMap.forEach((button, id) -> button.setOnAction(event -> {
-            addItem(id);
-            displaySelected(id);
-        }));
-
         place_order_btn.setOnAction(event -> {
             placeOrder();
             clearSelected();
@@ -105,48 +58,71 @@ public class ItemPickerController implements Initializable {
     }
 
     private void registerButtons() {
-        buttonIdMap.put(water_03_btn, 1);
-        buttonIdMap.put(water_05_btn, 2);
-        buttonIdMap.put(sparkling_water_03_btn, 3);
-        buttonIdMap.put(sparkling_water_05_btn, 4);
-        buttonIdMap.put(cola_btn, 5);
-        buttonIdMap.put(sprite_btn, 6);
-        buttonIdMap.put(fanta_btn, 7);
-        buttonIdMap.put(ice_tea_peach_btn, 8);
-        buttonIdMap.put(ice_tea_lemon_btn, 9);
-        buttonIdMap.put(energy_drink_btn, 10);
-        buttonIdMap.put(smoothie_btn, 11);
-        buttonIdMap.put(herbal_tea_btn, 12);
-        buttonIdMap.put(hot_chocolate_btn, 13);
-        buttonIdMap.put(espresso_btn, 14);
-        buttonIdMap.put(cappuccino_btn, 15);
+        buttonIdMap.put("Water 0.3", 1);
+        buttonIdMap.put("Water 0.5", 2);
+        buttonIdMap.put("Sparkling Water 0.3", 3);
+        buttonIdMap.put("sparkling water 05", 4);
+        buttonIdMap.put("cola", 5);
+        buttonIdMap.put("sprite", 6);
+        buttonIdMap.put("fanta", 7);
+        buttonIdMap.put("ice tea peach", 8);
+        buttonIdMap.put("ice tea lemon", 9);
+        buttonIdMap.put("energy drink", 10);
+        buttonIdMap.put("smoothie", 11);
+        buttonIdMap.put("herbal tea", 12);
+        buttonIdMap.put("hot chocolate", 13);
+        buttonIdMap.put("espresso", 14);
+        buttonIdMap.put("cappuccino", 15);
 
-        buttonIdMap.put(draft_beer_btn, 16);
-        buttonIdMap.put(wheat_beer_btn, 17);
-        buttonIdMap.put(aperol_spritz_btn, 18);
-        buttonIdMap.put(tequila_shot_btn, 19);
-        buttonIdMap.put(whisky_sour_btn, 20);
-        buttonIdMap.put(red_wine_btn, 21);
-        buttonIdMap.put(white_wine_btn, 22);
-        buttonIdMap.put(mojito_btn, 23);
-        buttonIdMap.put(gin_tonic_btn, 24);
+        buttonIdMap.put("draft beer", 16);
+        buttonIdMap.put("wheat beer", 17);
+        buttonIdMap.put("aperol spritz", 18);
+        buttonIdMap.put("tequila shot", 19);
+        buttonIdMap.put("whisky sour", 20);
+        buttonIdMap.put("red wine", 21);
+        buttonIdMap.put("white wine", 22);
+        buttonIdMap.put("mojito", 23);
+        buttonIdMap.put("gin tonic", 24);
 
-        buttonIdMap.put(cosmopolitan_btn, 25);
-        buttonIdMap.put(caipirinha_btn, 26);
-        buttonIdMap.put(long_island_iced_tea_btn, 27);
-        buttonIdMap.put(pina_colada_btn, 28);
+        buttonIdMap.put("cosmopolitan", 25);
+        buttonIdMap.put("caipirinha", 26);
+        buttonIdMap.put("long island iced tea", 27);
+        buttonIdMap.put("pina colada", 28);
 
-        buttonIdMap.put(burger_btn, 29);
-        buttonIdMap.put(burger_witch_fries_btn, 30);
-        buttonIdMap.put(chicken_wings_btn, 31);
-        buttonIdMap.put(nachos_cheese_btn, 32);
-        buttonIdMap.put(ceaser_salad_btn, 33);
-        buttonIdMap.put(mini_pizzas_btn, 34);
-        buttonIdMap.put(club_sandwich_btn, 35);
-        buttonIdMap.put(cheese_platter_btn, 36);
-        buttonIdMap.put(peanuts_btn, 37);
-        buttonIdMap.put(pretzel_sticks_btn, 38);
-        buttonIdMap.put(chips_btn, 39);
+        buttonIdMap.put("burger", 29);
+        buttonIdMap.put("burger witch fries", 30);
+        buttonIdMap.put("chicken wings", 31);
+        buttonIdMap.put("nachos cheese", 32);
+        buttonIdMap.put("ceaser salad", 33);
+        buttonIdMap.put("mini pizzas", 34);
+        buttonIdMap.put("club sandwich", 35);
+        buttonIdMap.put("cheese platter", 36);
+        buttonIdMap.put("peanuts", 37);
+        buttonIdMap.put("pretzel sticks", 38);
+        buttonIdMap.put("chips", 39);
+
+        // dynamically add the buttons to the grid
+        food_items.addRow(0);
+
+        AtomicInteger column = new AtomicInteger();
+        AtomicInteger row = new AtomicInteger();
+        buttonIdMap.forEach((item, id) -> {
+            Button button = new Button(item);
+            button.setOnAction(event -> {
+                // Set up event listener
+                addItem(id);
+                displaySelected(id);
+            });
+
+            if (column.get() == 6) {
+                column.set(0);
+                row.getAndIncrement();
+                food_items.addRow(row.get());
+            }
+
+            food_items.add(button, column.get(), row.get());
+            column.getAndIncrement();
+        });
     }
 
 
